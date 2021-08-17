979209b944f3baedb17bb51e96f72248341892ab
Alvin0183 <alvinmartinez666@outlook.com>
2021-08-16 22:27:15 -0600

    Actualizacion

---
+++ Persona/Persona/Negocio/NClsPersona.cs
@@ -0,0 +1,38 @@
+﻿using Persona.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persona.Negocio
+{
+    class NClsPersona
+    {
+        public String Reir(ClsPersona persona)
+        {
+            return " La persona de nombre " + persona.Nombre + " de edad " + persona.Edad + " Esta Riendo ";
+
+
+        }
+
+        public String Caminar(ClsPersona persona)
+        {
+            return " La persona de nombre " + persona.Nombre + " de edad " + persona.Edad + " Comenzo a caminar ";
+
+
+        }
+
+        public String Hablando(ClsPersona persona)
+        {
+            return " La persona " + persona.Nombre + " de sexo " + persona.Sexo  + " comenzo a hablar ";
+
+        }
+
+        public String Observar(ClsPersona persona)
+        {
+            return " La persona " + persona.Nombre + " de edad " + persona.Edad + " Te esta observando ";
+
+        }
+    }
+}
