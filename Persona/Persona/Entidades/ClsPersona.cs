3c264ade01ceacb2490a532bbecbfd0d9c247c56
Ariel123-beep <ariel1234alas@outlook.com>
2021-08-16 22:06:48 -0600

    Actualizacion

---
+++ Persona/Persona/Entidades/ClsPersona.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persona.Entidades
+{
+    class ClsPersona
+    {
+        private string nombre;
+        private string apellido;
+        private int edad;
+        private string sexo;
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+        public int Edad { get => edad; set => edad = value; }
+        public string Sexo { get => sexo; set => sexo = value; }
+    }
+}
